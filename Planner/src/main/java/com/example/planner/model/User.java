f2440a58496e388186a60965a7f8e3de2461256f
Kornelia <korneliamachnicka@kornelias-air.home>
2024-07-24 09:18:39 +0200

    fundamentals

---
+++ Planner/src/main/java/com/example/planner/model/User.java
@@ -0,0 +1,76 @@
+package com.example.planner.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Note> notes = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Event> events = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Task> tasks = new ArrayList<>();
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
